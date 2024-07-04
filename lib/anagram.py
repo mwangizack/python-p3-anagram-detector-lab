a13b24cca9097f2a9f07388a1bf89b2e83fbb92d
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-07-04 04:18:14 +0300

    fix: Define class Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, input_list):
+        result = []
+        word_letters = [letter for letter in self.word]
+        for input in input_list:
+            input_letters = [letter for letter in input]
+            if sorted(word_letters) == sorted(input_letters):
+                result.append(input)
+                    
+        return result
